44e8a8c5a150319d7d2d1b88286f13c7e6121ec3
Sven Frei <frei-sven@bluewin.ch>
2023-07-16 18:23:53 +0200

    Fixed issue with add vehicle servlet

--- project/src/java/controller/AddVehicleServlet.java
+++ project/src/java/controller/AddVehicleServlet.java
@@ -21,55 +21,6 @@ import javax.servlet.http.HttpServletResponse;
 public class AddVehicleServlet extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-         String noplate = request.getParameter("noplate");
-        
-           if (noplate != null){
-            
-            String vehicletype = request.getParameter("vehicletype");
-            
-            VehicleBean addvehicleBean = new VehicleBean(vehicletype, noplate);
-
-            if (RegisterVehicleDao.addvehicle(addvehicleBean)) {
-                request.getRequestDispatcher("/dashboard.jsp").forward(request, response);
-                return;
-            }
-            else {
-                request.setAttribute("errMessage", "Insert unsuccessful!");
-            }
-        }
-        else {
-                request.setAttribute("errMessage", "Please insert Vehicle Type");
-        }
-        
-        request.getRequestDispatcher("/regvehicle.jsp").forward(request, response);
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
      * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
@@ -80,11 +31,26 @@ public class AddVehicleServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-           
-       
-    }
-    
+        String noplate = request.getParameter("noplate");
 
+        if (noplate != null) {
+
+            String vehicletype = request.getParameter("vehicletype");
+
+            VehicleBean addvehicleBean = new VehicleBean(vehicletype, noplate);
+
+            if (RegisterVehicleDao.addvehicle(addvehicleBean)) {
+                request.getRequestDispatcher("/dashboard.jsp").forward(request, response);
+                return;
+            } else {
+                request.setAttribute("errMessage", "Insert unsuccessful!");
+            }
+        } else {
+            request.setAttribute("errMessage", "Please insert Vehicle Type");
+        }
+
+        request.getRequestDispatcher("/regvehicle.jsp").forward(request, response);
+    }
 
     /**
      * Returns a short description of the servlet.
@@ -94,6 +60,5 @@ public class AddVehicleServlet extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
-
+    }
 }
