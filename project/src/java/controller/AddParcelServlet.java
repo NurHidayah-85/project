2c3a9f9a429c7086fd68926cb1ac06d583ffff31
neallylove <107135328+NurHidayah-85@users.noreply.github.com>
2023-07-17 11:27:05 +0800

    up

--- project/src/java/controller/AddParcelServlet.java
+++ project/src/java/controller/AddParcelServlet.java
@@ -1,67 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import bean.RegInformationBean;
 import dao.RegParcelDao;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author LENOVO
- */
 public class AddParcelServlet extends HttpServlet {
- /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-     
-    }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         String custsname = request.getParameter("custsname");
         String custrname = request.getParameter("custrname");
-        
 
         if (custsname != null && custrname != null) {
             int custsphone = Integer.parseInt(request.getParameter("custsphone"));
@@ -72,17 +26,16 @@ public class AddParcelServlet extends HttpServlet {
             int custrposcode = Integer.parseInt(request.getParameter("custrposcode"));
             String custsemail = request.getParameter("custsemail");
             String custremail = request.getParameter("custremail");
-            
+
             double weight = Double.parseDouble(request.getParameter("weight"));
-             double height = Double.parseDouble(request.getParameter("height"));
-             String shiptype = request.getParameter("shiptype");
-             
-             int custsid = Integer.parseInt(request.getParameter("custsid"));
+            double height = Double.parseDouble(request.getParameter("height"));
+            String shiptype = request.getParameter("shiptype");
+
+            int custsid = Integer.parseInt(request.getParameter("custsid"));
             int custrid = Integer.parseInt(request.getParameter("custrid"));
             int staffid = Integer.parseInt(request.getParameter("staffid"));
-            
-            
-            RegInformationBean reginfoBean = new RegInformationBean (int custsid, String custsname, int custsphone, String custsadd, int custsposcode, String custsemail, int custrid, String custrname, int custrphone, String custradd, int custrposcode, String custremail, int itemid, double weight, double height, String shiptype, int staffidl, int custrid, String custrname, int custrphone, String custradd, int custrposcode, String custremail, int itemid, double weight, double height, String shiptype, int staffid);
+
+            RegInformationBean reginfoBean = new RegInformationBean(custsid, custsname, custsphone, custsadd, custsposcode, custsemail, custrid, custrname, custrphone, custradd, custrposcode, custremail, itemid, weight, height, shiptype, staffid);
 
             if (RegParcelDao.reginfo(reginfoBean)) {
                 request.getRequestDispatcher("/dashboard.jsp").forward(request, response);
@@ -97,15 +50,8 @@ public class AddParcelServlet extends HttpServlet {
         request.getRequestDispatcher("/regbranch.jsp").forward(request, response);
     }
 
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
-
+    }
 }
